be0d221ec48a75cabe3644d285063c4a34a31eda
toxtli <ctoxtli@gmail.com>
2020-03-18 00:25:24 -0400

    files updated

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import gdown
 
 if len(sys.argv) == 1:
 	print('The Google Colab URL is missing')
-else
+else:
 	url = sys.argv[1]
 	drive_id = url.split('/').pop()
 	url = 'https://drive.google.com/uc?id=' + drive_id

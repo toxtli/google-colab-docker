9361f9f0a2cfb91848e599d45998f414c4020682
toxtli <ctoxtli@gmail.com>
2020-03-18 00:25:04 -0400

    files updated

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
+import os
 import sys
-print(sys.argv)
+import gdown
+
+if len(sys.argv) == 1:
+	print('The Google Colab URL is missing')
+else
+	url = sys.argv[1]
+	drive_id = url.split('/').pop()
+	url = 'https://drive.google.com/uc?id=' + drive_id
+	output = 'out.ipynb'
+	gdown.download(url, output, quiet=False)
+	print(os.listdir())
